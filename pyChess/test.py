2ec32b4016950c6494ee0d3e7116301ccd248418
Pandozz <pandozgames@gmail.com>
2025-07-15 15:51:30 -0700

    Add bishop move test, set game_board as pytest.fixture.

--- pyChess/test.py
+++ pyChess/test.py
@@ -7,85 +7,79 @@ from game.pieces.queen import Queen
 from game.pieces.king import King
 from game.pieces.starting_pieces import starting_pieces
 
-starting_piece_list = []
+import pytest
 
-for piece in starting_pieces:
-    if piece['type'] == 'pawn':
-        starting_piece_list.append(Pawn(piece))
-    elif piece['type'] == 'knight':
-        starting_piece_list.append(Knight(piece))
-    elif piece['type'] == 'bishop':
-        starting_piece_list.append(Bishop(piece))
-    elif piece['type'] == 'rook':
-        starting_piece_list.append(Rook(piece))
-    elif piece['type'] == 'queen':
-        starting_piece_list.append(Queen(piece))
-    elif piece['type'] == 'king':
-        starting_piece_list.append(King(piece))
-    else:
-        raise ValueError(f"{piece['type']} is an invalid piece type.")
+
+@pytest.fixture
+def game_board():
+    starting_piece_list = []
+
+    for piece in starting_pieces:
+        if piece['type'] == 'pawn':
+            starting_piece_list.append(Pawn(piece))
+        elif piece['type'] == 'knight':
+            starting_piece_list.append(Knight(piece))
+        elif piece['type'] == 'bishop':
+            starting_piece_list.append(Bishop(piece))
+        elif piece['type'] == 'rook':
+            starting_piece_list.append(Rook(piece))
+        elif piece['type'] == 'queen':
+            starting_piece_list.append(Queen(piece))
+        elif piece['type'] == 'king':
+            starting_piece_list.append(King(piece))
+        else:
+            raise ValueError(f"{piece['type']} is an invalid piece type.")
+
+    return Board(starting_piece_list)
+
 
 class TestChess():
 
-    def __init__(self):
-        self.game_board = Board(starting_piece_list)
+    def test_pawn_move(self, game_board):
 
-    def test_pawn_move(self):
         starting_square = (2, 2)
         target_square = (4, 2)
-        pawn = self.game_board.get_occupant(starting_square)
+        pawn = game_board.get_occupant(starting_square)
         id = pawn.get_id()
 
-        pawn.move(target_square, self.game_board)
-        assert (self.game_board.is_occupied((target_square)))
-        assert (not self.game_board.is_occupied((starting_square)))
-        assert (self.game_board.get_occupant(target_square).get_id() == id)
+        pawn.move(target_square, game_board)
+        assert (game_board.is_occupied((target_square)))
+        assert (game_board.get_occupant(target_square).get_id() == id)
 
-pawn_a2 = game_board.get_occupant((2,2))
+        assert (not game_board.is_occupied((starting_square)))
+        assert (game_board.get_occupant((starting_square)) is None)
 
-pawn_a2.move((4,2), game_board)
-game_board.pretty_print_board_state()
+    def test_bishop_move(self, game_board):
+        starting_square = (1, 3)
+        open_square = (3, 1)
+        blocked_square = (8, 6)
 
-pawn_a2 = game_board.get_occupant((4,2))
+        game_board.pretty_print_board_state()
+        bishop = game_board.get_occupant(starting_square)
+        id = bishop.get_id()
 
-pawn_a2.move((5,2), game_board)
-game_board.pretty_print_board_state()
+        with pytest.raises(ValueError):
+            bishop.move(open_square, game_board)
 
-pawn_a2 = game_board.get_occupant((5,2))
+        game_board.pretty_print_board_state()
 
-pawn_a2.move((6,2), game_board)
-game_board.pretty_print_board_state()
+        starting_square2 = (2, 2)
+        target_square2 = (4, 2)
+        pawn = game_board.get_occupant(starting_square2)
+        pawn.move(target_square2, game_board)
 
-pawn_a2 = game_board.get_occupant((6,2))
+        game_board.pretty_print_board_state()
 
-pawn_a2.move((7,3), game_board)
-game_board.pretty_print_board_state()
+        bishop = game_board.get_occupant(starting_square)
+        bishop.move(open_square, game_board)
 
-pawn_a2 = game_board.get_occupant((7,3))
+        game_board.pretty_print_board_state()
+        assert (game_board.is_occupied((open_square)))
+        assert (game_board.get_occupant(open_square).get_id() == id)
 
-pawn_a2.move((8,2), game_board)
-game_board.pretty_print_board_state()
+        assert (not game_board.is_occupied((starting_square)))
+        assert (game_board.get_occupant((starting_square)) is None)
 
-bishop_c1 = game_board.get_occupant((1,3))
-bishop_c1.move((3,1), game_board)
-
-game_board.pretty_print_board_state()
-
-bishop_c1 = game_board.get_occupant((3,1))
-try:
-    bishop_c1.move((8,6), game_board)
-except ValueError as e:
-    print(f"Caught {e}")
-
-game_board.pretty_print_board_state()
-
-rook_a8 = game_board.get_occupant((8,1))
-rook_a8.move((8,2), game_board)
-game_board.pretty_print_board_state()
-
-rook_a8 = game_board.get_occupant((8,2))
-try:
-    rook_a8.move((4,2), game_board)
-except ValueError as e:
-    print(f"Caught {e}")
-game_board.pretty_print_board_state()
+        bishop = game_board.get_occupant(open_square)
+        with pytest.raises(ValueError):
+            bishop.move(blocked_square, game_board)
