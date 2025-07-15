3b47f7fe76ba9c4c9a198a0630feed1582b1247c
Pandozz <pandozgames@gmail.com>
2025-07-14 19:10:09 -0700

    Move piece.py to game folder, add draft Pawn class.

---
+++ pyChess/game/pieces/pawn.py
@@ -0,0 +1,49 @@
+from game.piece import Piece
+
+class Pawn(Piece):
+    
+    def __init__(self, config):
+        super().__init__(config)
+    
+        self.start_square = config.location
+    
+    def is_legal_move(self, end_square, board):
+        # Check if move is on the board
+        if end_square[0] not in board.get_rows():
+            return False
+        if end_square[1] not in board.get_columns():
+            return False
+        
+        # Handle first move
+        if self.location == self.start_square:
+            if end_square[0] != self.location[0]:
+                return False
+            if self.color == 'white':
+                if end_square[1] > self.location[1] + 2:
+                    return False
+            if self.color == 'black':
+                if end_square[1] < self.location[1] - 2:
+                    return False
+                
+        # Any move must be within +- 1 column
+        if end_square[0] > self.location[0] + 1 or end_square[0] < self.location[0] - 1:
+            return False
+        
+        # Any non-first move must be 1 row further
+        if self.color == 'white':
+            if end_square[1] != self.location[1] + 1:
+                return False
+        if self.color == 'black':
+            if end_square[1] != self.location[1] - 1:
+                return False
+
+        # Any non-attacking move must move to an unoccupied space
+        if end_square[0] == self.location[0]:
+            if board.is_occupied(end_square):
+                return False
+        # Any attacking move must move to an occupied space
+        else:
+            if not board.is_occupied(end_square):
+                return False
+            
+        return True
