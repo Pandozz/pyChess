e98fc8a3534c8eb76a8c985cf10b3027bc89b2b2
Pandozz <pandozgames@gmail.com>
2025-07-14 21:29:38 -0700

    Add board square attack status for checking valid king moves.

--- pyChess/game/board.py
+++ pyChess/game/board.py
@@ -6,14 +6,16 @@ class Board():
     def __init__(self, pieces = []):
         self.board_state = defaultdict(Piece)
         self.rows = self.columns = [i for i in range(1, 9)]
+        self.board_attack_status = defaultdict(set)
     
         for r in self.rows:
             for c in self.columns:
                 self.board_state[(r,c)] = None
+                self.board_attack_status[(r,c)] = set()
                 
         for piece in pieces:
             self.add_occupant(piece.get_position(), piece)
-            
+                        
     def validate_square(self, square):
         if square[0] not in self.rows or square[1] not in self.columns:
             raise ValueError(f"{chr(square[1])}, {square[0]}) is not a valid square.")
@@ -40,6 +42,11 @@ class Board():
         self.validate_square(square)
         self.board_state[square].kill()
         self.board_state[square] = None
+        
+    def attacking_square(self, square, color):
+        if color in self.board_attack_status[square]:
+            return True
+        return False
 
     def print_board_state(self):
         print(self.board_state)
